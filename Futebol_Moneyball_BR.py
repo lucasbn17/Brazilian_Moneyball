b8d20e77558a1ef00231aabc70ce29f2a5eea15f
Lucas B. Nunes <65828691+lucasbn17@users.noreply.github.com>
2022-11-20 16:47:15 -0800

    Update to texts and explanations

--- Futebol_Moneyball_BR.py
+++ Futebol_Moneyball_BR.py
@@ -45,6 +45,10 @@ def Similarity_performance(player_id1, player_id2):
 # Criando função de recomendação para jogadores de custo-benefísio mais semelhante
 def replace_player():
 
+      st.subheader("Tipo de recomendação: Similaridade em custo-benefício")
+      st.text("As recomendações apresentadas nesta página apontam quais jogadores mais se \naproximam em custo-benefício ao jogador escolhido.")
+      st.markdown("---")    
+    
       if name in df_knn_posição['player_name'].tolist() and equipe in df_knn_posição['club_names_tm'].tolist():
         new_player = df_knn_posição[(df_knn_posição['club_names_tm'] == equipe) & df_knn_posição['player_name'].str.contains(name)].iloc[0].to_frame().T
         id_player = new_player['player_id'].iloc[0]
@@ -116,6 +120,7 @@ def replace_player():
         df_recomend['Time'] = col_equipe
         df_recomend['Valor de Performance (€)'] = col_performance
         df_recomend['Valor de mercado (€)'] = col_custo
+        df_recomend.index += 1 
         st.dataframe(df_recomend)
         
         
@@ -145,6 +150,10 @@ def replace_player():
 # Criando função de recomendação para jogadores com performance mais semelhante
 def performance_substitute():
 
+  st.subheader("Tipo de recomendação: Similaridade em performance")
+  st.text("As recomendações apresentadas nesta página apontam quais jogadores possuem as \nperformances mais similares à do jogador escolhido.")
+  st.markdown("---")    
+    
   if name in df_knn_posição['player_name'].tolist() and equipe in df_knn_posição['club_names_tm'].tolist():
     new_player = df_knn_posição[(df_knn_posição['club_names_tm'] == equipe) & df_knn_posição['player_name'].str.contains(name)].iloc[0].to_frame().T
     id_player = new_player['player_id'].iloc[0]
@@ -215,6 +224,7 @@ def performance_substitute():
     df_recomend['Time'] = col_equipe
     df_recomend['Valor de Performance (€)'] = col_performance
     df_recomend['Valor de mercado (€)'] = col_custo
+    df_recomend.index += 1 
     st.dataframe(df_recomend)
     
     
@@ -243,6 +253,10 @@ def performance_substitute():
 # Criando função de recomendação para jogadores com performance mais semelhante e custo benefício mais atraente
 def interesting_options():
 
+  st.subheader("Tipo de recomendação: Outras recomendações")
+  st.text("Este tipo de recomendação mais inusitado mensura a diferença de custo-benefício \nentre todos os jogadores e o jogador escolhido e ranqueia quais destes jogadores \napresentam a melhor relação entre performance e custo-benefício.")
+  st.markdown("---")    
+    
   if name in df_knn_posição['player_name'].tolist() and equipe in df_knn_posição['club_names_tm'].tolist():
     new_player = df_knn_posição[(df_knn_posição['club_names_tm'] == equipe) & df_knn_posição['player_name'].str.contains(name)].iloc[0].to_frame().T
     id_player = new_player['player_id'].iloc[0]
@@ -313,6 +327,7 @@ def interesting_options():
     df_recomend['Time'] = col_equipe
     df_recomend['Valor de Performance (€)'] = col_performance
     df_recomend['Valor de mercado (€)'] = col_custo
+    df_recomend.index += 1 
     st.dataframe(df_recomend)
     
     
@@ -342,6 +357,10 @@ def interesting_options():
 # Criando função de recomendação com melhor custo benefício
 def best_performance_and_cost_benefit():
 
+  st.subheader("Tipo de recomendação: Melhor custo-benefício")
+  st.text("As recomendações apresentadas nesta página apontam quais jogadores possuem os \nmelhores custos-benefícios para esta posição.")
+  st.markdown("---")    
+    
   if name in df_knn_posição['player_name'].tolist() and equipe in df_knn_posição['club_names_tm'].tolist():
     new_player = df_knn_posição[(df_knn_posição['club_names_tm'] == equipe) & df_knn_posição['player_name'].str.contains(name)].iloc[0].to_frame().T
     id_player = new_player['player_id'].iloc[0]
@@ -412,6 +431,7 @@ def best_performance_and_cost_benefit():
     df_recomend['Time'] = col_equipe
     df_recomend['Valor de Performance (€)'] = col_performance
     df_recomend['Valor de mercado (€)'] = col_custo
+    df_recomend.index += 1 
     st.dataframe(df_recomend)
     
     
@@ -464,14 +484,14 @@ def comp_graph(player_id) :
                 width=2
             )
         )))
-   
-  #   fig.update_layout(legend=dict(
+
+#   fig.update_layout(legend=dict(
 #     yanchor="bottom",
 #     y=0.01,
 #     xanchor="right",
 #     x=0.99
 #     ))
-
+   
   return fig
   
 #########################################################################################################
@@ -481,12 +501,10 @@ st.title("Granabola")
 
 
 #df usado nas ferramentas de distância entre pontos
-# df_knn = pd.read_csv(r'C:\Users\User\TERA - Python Exercises\Project TERA\df_knn.csv')
-df_knn = pd.read_csv(r'df_knn.csv')
+df_knn = pd.read_csv(r'C:\Users\User\TERA - Python Exercises\Project TERA\df_knn.csv')
 
 #df usado na tabela comparativa de estatísticas da interface
-# df_2022 = pd.read_csv(r'C:\Users\User\TERA - Python Exercises\Project TERA\df_2022.csv')
-df_2022 = pd.read_csv(r'df_2022.csv')
+df_2022 = pd.read_csv(r'C:\Users\User\TERA - Python Exercises\Project TERA\df_2022.csv')
 df_2022.set_index('player_name', inplace=True)
 df_2022.drop(['Unnamed: 0', 'player_firstname', 'player_lastname', 'player_injured',
               'statistics_league_season', 'statistics_games_position', 'statistics_games_rating',
@@ -576,8 +594,13 @@ if recomendação != None:
         st.markdown("![Alt Text](https://media.giphy.com/media/8rEVtjWcJoNI5miInv/giphy.gif)")
 else:
     st.subheader("O Moneyball versão brasileira Herbert Richers")
-    st.text("Este app identifica eventuais oportunidades de negócio no futebol brasileiro")
+    st.text("Este app identifica eventuais oportunidades de negócio no futebol brasileiro.")
     st.markdown("![Alt Text](https://i.gifer.com/CbnC.gif)")
+    st.text("Instruções:")
+    st.text("1. Utilize o menu do lado esquerdo para selecionar um jogador como referência.")
+    st.text("2. Selecione o tipo de recomendação desejada.")
+    st.text("3. O aplicativo abrirá a página de recomendação.")
+    st.text("4. Gere o gráfico de pontos e a tabela de performance comparativa entre\n jogadores para uma melhor análise.")
     botão_saber_mais = False
     new = 2
     botão_saber_mais = st.button('Saber Mais')
